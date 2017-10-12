360d036ceb472c1f25ae4100103976b26b01ae5f
B Kleinen <kleinen@infrastructure.de>
2017-10-12 13:00:15 +0200

    example made in class 12.10.2017

--- about/arrays.rb
+++ about/arrays.rb
@@ -1,13 +1,19 @@
 describe "Arrays" do
 
-	let(:arrayX) {[0,1,2,3]}
+	let(:arrayX) {[1,2,3,4]}
 	let(:arrayStr) {["Name","Nachname","Vorname"]}
 
-	it "elements can be accessed" do
-		expect(arrayX[0]).to eq(0)
+	it "indices start at 0" do
+		expect(arrayX[2]).to eq(3)
 	end
 
-	it "adding elements"
+	it "adding elements" do
+		arrayX.unshift(7)
+		puts arrayX.inspect
+		expect(arrayX[0]).to eq(7)
+		expect(arrayX.size).to eq(5)
+
+	end
 
 
 	it "deleting element"
