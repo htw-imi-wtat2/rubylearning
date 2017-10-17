822b8a621008c4bea00fb13e6a507b6ddb18c01d
Anonymous31337 <kevinwrede@gmx.net>
2017-10-17 13:41:47 +0200

    remove/add test added

--- about/arrays.rb
+++ about/arrays.rb
@@ -12,10 +12,32 @@ describe "Arrays" do
 		puts arrayX.inspect
 		expect(arrayX[0]).to eq(7)
 		expect(arrayX.size).to eq(5)
+	end
+
+	it "string indices start at 0" do
+		expect(arrayStr[2]).to eq("Vorname")
+	end
+
+	it "adding string elements" do
+		arrayStr.unshift("Telefon")
+		puts arrayStr.inspect
+		expect(arrayStr[0]).to eq("Telefon")
+		expect(arrayStr.size).to eq(4)
 
 	end
 
+	it "deleting element" do
+		arrayX.shift(0)
+		puts arrayX.inspect
+		expect(arrayX[0]).to eq(1)
+		expect(arrayX.size).to eq(4)
+	end	
+	
+	it "deleting string elements" do
+		arrayStr.shift(0)
+		puts arrayStr.inspect
+		expect(arrayStr[0]).to eq("Name")
+		expect(arrayStr.size).to eq(3)
 
-	it "deleting element"
-
+	end	
 end
